333b8c876b71498d04568d4ebe95783e9b241420
nikhil <sasaniki@users.noreply.github.com>
2017-08-19 21:18:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,51 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articleone={
+  title:'article-one',
+  heading:'articleone',
+  date:'sep 5,2017',
+  content:` <h2>Personal</h2>
+        <p>
+             This is some personal information   about me.
+        </p>
+        <h2>Professional</h2>
+        <p>
+             This is a list of my work experiences:
+        </p>
+        <ol>
+         <li> Company A: Worked as some very seriously</li>
+         <li> Company B: Worked without seriousness </li>
+        </ol>`
+};
 
+function createtem(data){
+var title=data.title;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
+var htmltemplate=`<html>
+    <head>
+        <meta name="viewport" content="width=device-width , initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">HOME</a>
+        </div>
+        <h1>$[heading]</h1>
+        $[articleone]
+        </div>
+    </body>
+</html>
+`;
+return htmltemplate;
+}
+
+app.get('/ui/articleone', function (req, res) {
+    res.send(createtem(articleone));
+});
 
 app.get('/ui/articletwo', function (req, res) {
     res.send('SERVED SOON');
