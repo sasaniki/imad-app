f8b5dda046d011061d5e28c2e5273b84fdc4abd5
nikhil <sasaniki@users.noreply.github.com>
2017-08-19 22:33:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ function createtem(data){
 return htmltemplate;
 }
 
-app.get(':/articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
     var articlename=req.params.articleName;
     res.send(createtem(articles[articlename]));
     //res.send(createtem(articles['article-one']));
